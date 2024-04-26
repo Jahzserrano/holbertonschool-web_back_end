81fff6ea00d00d7bdc80222f5fd22993eccdcb13
Jahaziel Serrano <serranojahaziel2@gmail.com>
2024-04-26 18:07:01 -0400

    fixing

--- python_variable_annotations/0-add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Basic annotations - add"""
-
-
-def add(a: float, b: float) -> float:
-    """function with two parameters with float type annotations"""
-    return a + b
