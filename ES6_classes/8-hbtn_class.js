38c7931cf1eb8a8abf353a86ab25a745898514fa
Jahaziel Serrano <serranojahaziel2@gmail.com>
2024-05-10 22:20:43 -0400

    fixing bugs and eslint things

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -7,6 +7,6 @@ export default class HolbertonClass {
   [Symbol.toPrimitive](hint) {
     if (hint === 'string') return this._location;
     if (hint === 'number') return this._size;
-      return 'default';
+    return 'default';
   }
 }
