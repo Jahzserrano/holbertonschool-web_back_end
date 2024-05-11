b5db7e905a65d7f5581f2a048df532f7059fe0bd
Jahaziel Serrano <serranojahaziel2@gmail.com>
2024-05-10 22:06:51 -0400

    fixing bugs and eslint things

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,17 @@
 export default class Building {
-    constructor(sqft) {
-      if (
-        this.constructor !== Building
-        && typeof this.evacuationWarningMessage !== 'function'
-      ) {
+  constructor(sqft) {
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
         throw Error(
           'Class extending Building must override evacuationWarningMessage',
         );
       }
       this._sqft = sqft;
-    }
-  
-    get sqft() {
-      return this._sqft;
-    }
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
